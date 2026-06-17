5dca858e72ffc61bf0e544c330351183be1f0856
Tejas Yadav <tejassyadav3020@gmail.com>
2026-06-17 13:44:45 +0530

    All Recursion Levels

---
+++ core_java_practice/gcr-codebase/Recursion/Factorial.java
@@ -0,0 +1,26 @@
+package coreJavaPractice.gcr_codebase.Recursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static long factorial(int n) {
+
+        if (n <= 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(factorial(n));
+
+        sc.close();
+    }
+}
---
+++ core_java_practice/gcr-codebase/Recursion/sumN.java
@@ -0,0 +1,26 @@
+package coreJavaPractice.gcr_codebase.Recursion;
+
+import java.util.Scanner;
+
+public class sumN {
+
+    public static int sum(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(sum(n));
+
+        sc.close();
+    }
+}
